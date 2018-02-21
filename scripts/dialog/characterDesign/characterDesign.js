aa5477dd2fbaa638e37da3f8c4f347249b0a1668
saberking <saberking321@gmail.com>
2018-02-21 17:16:47 +0530

    foo

--- scripts/dialog/characterDesign/characterDesign.js
+++ scripts/dialog/characterDesign/characterDesign.js
@@ -70,8 +70,8 @@ const openLevelDialog=()=>{
   increasedStat[0]=[]
   // document.getElementById('continue').innerHTML='<div style="height:52"></div>'
   document.getElementById('dialog').innerHTML=
-  header(c)+displayStats(c)+'<div style="position:absolute;left:200;top:50"id="stats"></div>'
-  document.getElementById('stats').innerHTML=  displaySkills(c)+'<div id="continue_0"></div>'
+  header(c)+displayStats(c)+'<div style="position:absolute;left:500;top:150"id="skills"></div>'
+  document.getElementById('skill').innerHTML=  displaySkills(c)+'<div id="continue_0"></div>'
   addSkillButtons()
   addStatButtons()
 }
